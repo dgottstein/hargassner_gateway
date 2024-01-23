f4f9a4007cbf391707f5779d7372ee154495c55f
Dominik Gottstein <gottstein.dominik@googlemail.com>
2024-01-23 15:39:17 +0100

    Startskripte aufgeräumt.

--- parse_and_log_mqtt.py
+++ parse_and_log_mqtt.py
@@ -2,6 +2,7 @@ import hargassner
 import sys, traceback
 import socket
 
+# MQTT credentials
 mqtt_cred = { "broker": "nas.stein",
               "port": 1883,
               "username": "Hargassner_Gateway",
@@ -12,12 +13,7 @@ mqtt_cred = { "broker": "nas.stein",
 print("Parsing header information from 'test\\xml\\Full.DAQ'", flush=True)
 channel_infos = hargassner.parse_header_information("test\\xml\\Full.DAQ")
 
-#print("Importing config file from 'channel_config.json'", flush=True)
-#channel_config = hargassner.import_config_file("channel_config.json")
-
 print("Connecting to and logging data from 'HSV1'", flush=True)
-#hargassner.connect_and_log_data(ip_address='HSV1', channel_infos=channel_infos, channel_config=channel_config, sql_connection=db)
-#hargassner.connect_and_log_data_influx(ip_address='HSV1.stein', channel_infos=channel_infos, channel_config=channel_config, influx_credentials=influx_cred)
 hargassner.connect_and_log_data_mqtt(ip_address='HSV1.stein', channel_infos=channel_infos, mqtt_credentials=mqtt_cred)
 
 print("Closing connection", flush=True)
